11453dc50ae9ab09e92ecf3b0b6e1aa79eba3c6e
logancheg1 <61198537+logancheg1@users.noreply.github.com>
2020-02-19 10:53:34 +0300

    1
    1

---
+++ privacy.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Privacy Policy</title>
+</head>
+<body>
+Privacy Policy<br>
+This document sets out how we use, process and protect your personal data you give us when using this website and ABC Mobile OÜ services/products and other content. We act in our customers' interest and are transparent about the processing of your personal data.<br>
+That means we are committed to protecting and safeguarding any personal data you give us. As an added bonus, it also tells you how to contact us if you have questions about your personal data. This one Privacy Policy applies to any kind of information we collect through our own website/apps, as well as through social media or our partners’ websites.<br>
+We might amend the Privacy Policy from time to time. If you care about your privacy, visit this page regularly to know exactly where you stand. If we make changes to the Privacy Policy which will have an impact on you (e.g. if we intend to process your personal data for other purposes than communicated in the past in this Privacy Policy), we'll notify you of these changes before the new activities begin.<br>
+The hard truth: Your use of this website and/or our services/products here constitute your consent to the terms of this Privacy Policy. If you disagree with this Privacy Policy, you should discontinue using our services/products and this website.<br>
+Any Information provided by you with which you can be identified when using this website and our services/products, will be used in accordance with this Privacy Policy. This Policy sets out the following:<br>
+1. What information we collect<br>
+2. How the information is used by us<br>
+3. How do we store and maintain data<br>
+4. Disclosure<br>
+5. Cookies<br>
+6. Your rights<br>
+7. Contact details<br>
+What information we collect<br>
+The information collected by us is necessary to provide you with a service/product and/or to allow us to optimize our services/products. Such information can be collected when you use this website or when you use each of our services including our premium rate services and/or products.<br>
+The information we collect may include:<br>
+- Your name<br>
+- Gender<br>
+- Date/Time of birth (age)<br>
+- Email address<br>
+– Telephone number.<br>
+In some cases, we may collect and process the following information (depending on the type of service and/or product): your habits (including harmful addictions); hobbies and fancies; food excesses; clothing, pastime preferences and etc.; the favorite music; sport and sporting performance; your skills and experiences; personality traits and personal skills; educational background; relationship status; place of living (city, country); professional occupation, place of work/study; fears; the favorite color, country, city and other preferences, likes and dislikes; general information about wellbeing (NOTE: We collect and use only general information about your health, excluding data on diagnosis, diseases, tests, mental state and other anamnesis data) and other general data.<br>
+In addition to this, we also collect automatically information from your phone tablet/mobile device or other device you're using to access our website, services and/or products. This can include the IP address, data that identifies your mobile device, device-specific settings and characteristics, the browser you're using, app crashes, social network account, and other system activity, your language settings and location details. There are also situations when we receive information about you from others or automatically collect other information.<br>
+Our premium rate service can store your mobile phone number, the number you dialed, the length and frequency of your calls. We also store information on any interactions via social media platforms or forums. In addition to that, you can provide us with feedback or ask for help with using our support service, which involves providing us with personal data.<br>
+DISCLAMER: ALL INFORMATION AND/OR “ADVICES”, “TESTS RESULTS”, “PREDICTIONS” CONTAINED ON OUR WEBSITE, APPS, SERVICES/PRODUCTS ARE PROVIDED FOR ENTERTAINMENT PURPOSES ONLY, AND SHOULD NOT BE CONSTRUED AS MEDICAL, LEGAL, OR FINANCIAL ADVICE ON ANY SUBJECT MATTER. THE ABC MOBILE OÜ DISCLAIMS ALL LIABILITY IN REGARDS TO ACTIONS TAKEN OR NOT TAKEN BASED ON ANY OR ALL OF THE CONTENTS ON THIS WEBSITE. PLEASE NOTE THAT ABC MOBILE OÜ DOES NOT NECESSARILY ENDORSE, AND IS NOT RESPONSIBLE FOR, ANY THIRD-PARTY CONTENT THAT MAY BE ACCESSED THROUGH THIS WEBSITE.<br>
+<br>
+How the information is used by us<br>
+The information is used by us to:<br>
+- Provide the best service.<br>
+We provide our services/products from our local offices in different languages, and we’re here to help 24 hours a day, 7 days a week. Sharing your relevant details our customer service staff allows us to respond when you need us – including responding to any questions you might have about the services/products.<br>
+- Personalize the service to you<br>
+The main goal here is to optimize and customize our services/products to your needs, making them easier and more enjoyable to use.<br>
+- Verify that you are above the age limit to use the service.<br>
+The services/products offered by ABC Mobile OÜ are not directed at children under 18 years old. For children younger than 18 years old, the use of any of our services/products is only allowed with the valid consent of a parent or a guardian. If we become aware that we process information of a child under 18 years old without the valid consent of a parent or guardian, we reserve the right to delete it.<br>
+- Interact with you and update you on news, important notices (offers & promotions) via email, mail, phone or text message/SMS. When you use our services/products, we can send you the message with “login” and “password” to access the service/product or apps.<br>
+In addition to that we might send you a questionnaire or invite you to submit a review about your experience with our services/products and other content. If you do not wish to be contacted about our offers, promotions or news, you can unsubscribe from our emails, text message/SMS.<br>
+- Prevent fraud (Promotion of a safe and trustworthy service).<br>
+In order to create a trustworthy environment for you, we might use personal data for the detection and prevention of fraud and other illegal or unwanted activities. Similarly, we might use personal data for risk assessment and security purposes, including the authentication of users. For such purposes, we might have to stop certain services or put services on hold until we've finished our assessment.<br>
+We may also use and process your information and content, using different technologies to the following purposes:<br>
+To enforce the Terms of Use or Privacy Policy;<br>
+To contact you when we believe it to be necessary;<br>
+To detect abuse and illegal activity; to detect and prevent misappropriation, infringements, identity theft and other illegal activities and misuse of the Services and/or Products;<br>
+To handle breakdowns and malfunctions;<br>
+To take any action in any case of dispute, or legal proceeding of any kind between you and the Services and/or Products, or between you and other users or third parties with respect to, or in relation with the Services and/or Products;<br>
+For purposes provided under this Privacy Policy and Terms of Use;<br>
+To help improve the safety and reliability of our Services and Products. This includes detecting, preventing, and responding to security risks, and technical issues that could harm us, our users, or the public;<br>
+- Comply with regulatory requirements.<br>
+Personal data submitted is sent to and stored on secure servers located in the European Economic Area according to REGULATION (EU) 2016/679 OF THE EUROPEAN PARLIAMENT AND OF THE COUNCIL of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data.<br>
+- Research, analyze, update or enhance the services we provide.<br>
+We also use personal data for analytical purposes and measurement to understand how our services are used. This is part of our mission to improve our services and enhance the user experience, but can also be used for testing purposes, troubleshooting, and improving the functionality and quality of our services/products and content. We strive to only use pseudonymized data for these analytical purposes.<br>
+We may also monitor and record calls you make or receive when using our services for the purposes of quality control (Call monitoring). And if you contact us, we may keep a record of your request in order to help solve any issues you might be facing.<br>
+<br>
+We’ll ask for your consent before using your information for a purpose that isn’t covered in this Privacy Policy.<br>
+<br>
+3. How we store and maintain data<br>
+We observe reasonable procedures to prevent unauthorized access to, and the misuse of, personal data.<br>
+We use appropriate business systems and procedures to protect and safeguard the personal data you give us. We also use security procedures and technical and physical restrictions for accessing and using the personal data on our servers. Only authorized personnel are permitted to access personal data in the course of their work.<br>
+We will retain your personal data for as long as we deem it necessary to enable you to use our services/products, to provide our services to you (including maintaining the online user account (if created)), to comply with applicable laws, resolve disputes with any parties and otherwise as necessary to allow us to conduct our business, including to detect and prevent fraud or other illegal activities. All personal data we retain will be subject to this Privacy Policy. If you have a question about a specific retention period for certain types of personal data we process about you, please contact us using the contact details provided below.<br>
+<br>
+We process your information for the purposes described in this policy, based on the following legal grounds:<br>
+With your consent<br>
+<br>
+Disclosure of information<br>
+Information submitted may be transferred by us to our affiliated companies and/or to the third parties, which may be situated outside the European Economic Area and may be processed by staff operating outside the European Economic Area. By submitting information via the Website, you agree to this storing, processing and/or transfer. We may from time to time employ other companies and individuals to carry out functions on our behalf including, but not limited to service delivery, marketing assistance, data analysis and customer service provision, which may require access to your personal information. Where requested by law, we may disclose information to regulators or governing bodies. We disclose personal data to law enforcement insofar as it is required by law or is strictly necessary for the prevention, detection or prosecution of criminal acts and fraud or if we're otherwise legally obliged to do so. We may need to further disclose personal data to competent authorities to protect and defend our rights or properties, or the rights and properties of our business partners.<br>
+<br>
+Cookies<br>
+Cookies allow us to analyze traffic, for advertisement purposes and to better customize and personalize your experience at our Website by storing your personal preferences. We may collect the  information from you such IP address, device ID, browser type, time and/or length of your visit and/or call, the pages of the Website you visit, viewed pages, browsing info, operating system, internet service provider, carrier, timestamp, whether you have responded to an advertisement, the referring URL, and features used or activities engaged in within the website/apps and other data. We use cookies to validate the authenticity of Website visitors, provide personalized service for you, and enable traffic monitoring. Most web browsers automatically accept cookies and similar technologies, but if you prefer, you can change your browser to prevent that.<br>
+<br>
+Your rights<br>
+You have the right to know what personal information we hold about you, to correct errors in that information and to prevent us from using your personal data for marketing purposes.<br>
+You can ask us for a copy of the personal data we hold about you.<br>
+In certain situations, you can also ask us to send the personal data you've given us to a third party.<br>
+Where we use your personal data on the basis of your consent, you are entitled to withdraw that consent at any time subject to applicable law. Moreover, where we process your personal data based on legitimate interest or the public interest, you have the right to object at any time to that use of your personal data subject to applicable law.<br>
+For any of your requests in regards to this Privacy Policy and to exercise any of your rights or if you have a complaint, you can contact our Data Protection Officer at  support@abcmobile.com for further details.<br>
+If you want to object to the processing of your personal data on the basis of legitimate interest and no opt-out mechanism is available to you directly, you can contact us at support@abcmobile.com.<br>
+Contact details<br>
+ABC Mobile OÜ controls the processing of personal data as described in this Privacy Policy. ABC Mobile OÜ is a company, incorporated under the laws of Estonia with the registration number 14710834 and registered address at Harju maakond, Tallinn, Peterburi tee 71-318, 11415, Estonia.<br>
+If you have any questions about this Privacy Policy or our processing of your personal data, or if you believe that your privacy has been compromised in the course of using the Services and/or Products, please contact our data protection officer at support@abcmobile.com and we’ll get right back to you.<br>
+<br>
+</body>
+</html>
---
+++ terms.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Terms and Conditions</title>
+</head>
+<body>
+Terms and Conditions<br>
+This document outlines the Terms and Conditions governing the use of the products and/or services (hereafter referred as the Services) offered by ABCMobile OÜ, a company, incorporated under the laws of Estonia with the registration number 14710834 and registered address at Harju maakond, Tallinn, Peterburi tee 71-318, 11415, Estonia, and the user of the Website. By using the Services and/or installing any of ABC Mobile products, the user confirms that he or she is over the age of 18 and accepts to be legally bound by these Terms and Conditions, as well as of our Privacy Policy. For any comment or enquiry, please send an email to support@abcmobile.com.<br>
+General Terms<br>
+The Service makes no representations or warranties to the accuracy, relevance, or quality of the information and shall be considered for entertainment purposes only. The Service does not assume and hereby disclaims any liability to any person or entity for any loss or damages (including, but not limited to, physical, mental, special or indirect) caused in regard any information and/or any suggestion and advice provided to you through this Service, or as a result of your use of this Service, regardless of whether resulting from negligence, accident, or any other cause whatsoever<br>
+Health and/or medical related advice given should be checked out by a licensed medical practitioner before being acted upon.<br>
+Premium Rate Telephone Services<br>
+ABCMobile OÜ is registered with Phone-paid Services Authority (the Registration ID is ORG835-75235-94022), who regulate the use of premium rate telephone services (for further information, see www.phonepayplus.org.uk).<br>
+To use these services, you must either be the bill payer or have the bill payer’s permission to make the call. Calls to our Service from within the UK cost £3.60 per minute plus your phone company’s access charge. Maximum call length 30 minutes. Calls are recorded, for entertainment only, 18+.<br>
+Disclaimer, Copyright and Other Conditions<br>
+All products and Services are subject to copyright protection and should not be copied, shared, edited, modified or distributed in any way.<br>
+ABC Mobile OÜ reserves the right to modify or discontinue, temporarily or permanently, the Services provided to you if:<br>
+- you have misused our services in anyway;<br>
+- have been abusive to company employees or readers;<br>
+- has committed an act of fraud.<br>
+ABCMobile OÜ reserves the right to modify or change any products or services as required in order to improve the Service. We recommend to regularly check the site for updates to the services on offer.<br>
+This site may contain links which are hosted by third parties and are not associated with ABCMobile OÜ. The content displayed on those sites is not the responsibility of ABCMobile OÜ and we do not endorse these websites or services.<br>
+You hereby agree to fully indemnify and hold harmless ABCMobile OÜ, their officers, directors, employees, agents and service providers for any cost, expenses, loss, damages, claims and liabilities caused that may arise in relation to your use of the ABCMobile site or participation in the service.<br>
+Governing Law<br>
+These Terms and Conditions are governed by Estonian law and you and ABCMobile OÜ agree to submit to the exclusive jurisdiction of the Courts of Estonia.<br>
+
+</body>
+</html>
